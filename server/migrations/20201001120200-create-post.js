ba3c567676d7995fd5a537009fd53c2b4262bfcc
zanadh <fadzim018@gmail.com>
2020-10-01 21:04:12 +0700

    1. add CRUD to postController.js 2. add restful route to postRoute.js 3. add migration and seeder for table "Posts"

--- server/migrations/20201001120200-create-post.js
+++ server/migrations/20201001120200-create-post.js
@@ -15,7 +15,7 @@ module.exports = {
         type: Sequelize.STRING
       },
       story: {
-        type: Sequelize.STRING
+        type: Sequelize.TEXT
       },
       songs: {
         type: Sequelize.STRING
--- server/routes/postRoute.js
+++ server/routes/postRoute.js
@@ -3,5 +3,9 @@ const PostController = require("../controllers/postController")
 const router = require("express").Router()
 
 router.get("/", PostController.getAllPost)
+router.get("/:id", PostController.getPost)
+router.post("/add", PostController.postAdd)
+router.put("/edit/:id", PostController.putPost)
+router.delete("/delete/:id", PostController.deletePost)
 
 module.exports = router
