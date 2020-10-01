7c8dbbdd7db5a9599f12e076d908e48f6ca75846
zanadh <fadzim018@gmail.com>
2020-10-02 03:21:25 +0700

    Add OAUTH Google 1. add route to /route/index.js 2. add googleLogin method to apiController.js

--- server/controllers/apiController.js
+++ server/controllers/apiController.js
@@ -1,4 +1,8 @@
 const axios = require("axios")
+const {OAuth2Client} = require('google-auth-library');
+const client = new OAuth2Client('841246734810-ph9ikv3p8ae847gkere5m2b359glabpe.apps.googleusercontent.com');
+const {User} = require("../models")
+const {signToken} = require("../helpers/jwt")
 const weatherStack = axios.create({
     baseURL: 'http://api.weatherstack.com/current',
     params:{
@@ -21,6 +25,37 @@ class ApiController{
                 console.log(err);
                 res.status(500).json(err)
             })
+    }
+
+
+    static postGoogleLogin(req,res,next){ 
+        let token =  req.body.id_token
+        // console.log(token);  
+        let randomedPass = Math.round(Math.random()*100000)+1000000
+        // console.log(randomedPass);
+        client.verifyIdToken({idToken:token})
+            .then(payload=>{
+                // console.log(payload.payload);
+                return User.findOrCreate({ 
+                    defaults: {
+                    fullname: payload.payload.name, 
+                    email: payload.payload.email,
+                    password: randomedPass+""
+                    },
+                    where: {
+                    email:payload.payload.email
+                    }
+                })
+            })
+            .then(user=>{ 
+                let userData = {id:user[0].id ,fullname: user[0].fullname,email:user[0].email}
+                // console.log(userData);
+                let token = signToken(userData)
+                res.status(200).json({"access_token":token}) 
+            })
+            .catch(err=>{
+                next(err) 
+            }) 
     }
 }
 
